a0f3298a415bdfad2ff0ba3282c2aba66c2b9194
Ashish Agrawal <143439220+Ashishagrawal125@users.noreply.github.com>
2025-08-27 12:54:58 +0530

    Create 10-RegularExpressionMatching

---
+++ 10-RegularExpressionMatching/10-RegularExpressionMatching.java
@@ -0,0 +1,28 @@
+// Last updated: 8/27/2025, 12:54:58 PM
+class Solution {
+    public static boolean isMatch(String s, String p) {
+
+        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+        dp[0][0] = true;
+
+        for (int j = 2; j <= p.length(); j++)
+            if (p.charAt(j - 1) == '*')
+                dp[0][j] = dp[0][j - 2];
+
+        for (int i = 1; i <= s.length(); i++)
+            for (int j = 1; j <= p.length(); j++) {
+                if (p.charAt(j - 1) == '.' || p.charAt(j - 1) == s.charAt(i - 1))
+                    dp[i][j] = dp[i - 1][j - 1];
+                else if (p.charAt(j - 1) == '*')
+                    dp[i][j] = dp[i][j - 2] || ((p.charAt(j - 2) == '.' || p.charAt(j - 2) == s.charAt(i - 1)) && dp[i - 1][j]);
+            }
+
+        return dp[s.length()][p.length()];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isMatch("aa", "a*"));         // true
+        System.out.println(isMatch("ab", ".*"));         // true
+        System.out.println(isMatch("mississippi", "mis*is*ip*.")); // true
+    }
+}
