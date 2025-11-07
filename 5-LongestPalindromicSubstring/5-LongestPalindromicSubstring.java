bb5b19da1d207a04e489cd2d57e9bb4465fa6615
Ashish Agrawal <143439220+Ashishagrawal125@users.noreply.github.com>
2025-11-07 23:57:36 +0530

    File updated at 11/7/2025, 11:57:37 PM

--- 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.java
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.java
@@ -1,37 +1,25 @@
-// Last updated: 8/27/2025, 12:55:03 PM
+// Last updated: 11/7/2025, 11:57:37 PM
 class Solution {
-    
-
-    public static String longestPalindrome(String s) {
+    public String longestPalindrome(String s) {
         if (s == null || s.length() < 1) return "";
-
         int start = 0, end = 0;
-
         for (int i = 0; i < s.length(); i++) {
-            int len1 = expandAroundCenter(s, i, i);       // Odd length
-            int len2 = expandAroundCenter(s, i, i + 1);   // Even length
+            int len1 = expand(s, i, i);
+            int len2 = expand(s, i, i + 1);
             int len = Math.max(len1, len2);
-
             if (len > end - start) {
                 start = i - (len - 1) / 2;
                 end = i + len / 2;
             }
         }
-
         return s.substring(start, end + 1);
     }
 
-    private static int expandAroundCenter(String s, int left, int right) {
+    private int expand(String s, int left, int right) {
         while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
             left--;
             right++;
         }
         return right - left - 1;
     }
-
-    public static void main(String[] args) {
-        String input = "babad";
-        System.out.println("Longest Palindromic Substring: " + longestPalindrome(input));
-    }
 }
-
