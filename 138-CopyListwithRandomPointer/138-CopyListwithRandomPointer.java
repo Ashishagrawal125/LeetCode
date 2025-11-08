5ee551162dde2aa930e3210caf46f65d4285afeb
Ashish Agrawal <143439220+Ashishagrawal125@users.noreply.github.com>
2025-11-08 23:46:56 +0530

    Create 138-CopyListwithRandomPointer

---
+++ 138-CopyListwithRandomPointer/138-CopyListwithRandomPointer.java
@@ -0,0 +1,51 @@
+// Last updated: 11/8/2025, 11:46:56 PM
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+public class Solution {
+    public Node copyRandomList(Node head) {
+        if (head == null) return null;
+        Map<Node, Node> map = new HashMap<>();
+        Node curr = head;
+        while (curr != null) {
+            map.put(curr, new Node(curr.val));
+            curr = curr.next;
+        }
+        curr = head;
+        while (curr != null) {
+            map.get(curr).next = map.get(curr.next);
+            map.get(curr).random = map.get(curr.random);
+            curr = curr.next;
+        }
+        return map.get(head);
+    }
+
+    public static void main(String[] args) {
+        Node n1 = new Node(7);
+        Node n2 = new Node(13);
+        Node n3 = new Node(11);
+        Node n4 = new Node(10);
+        Node n5 = new Node(1);
+        n1.next = n2; n2.next = n3; n3.next = n4; n4.next = n5;
+        n2.random = n1; n3.random = n5; n4.random = n3; n5.random = n1;
+        Solution s = new Solution();
+        Node copy = s.copyRandomList(n1);
+        Node temp = copy;
+        while (temp != null) {
+            System.out.println("Val: " + temp.val + ", Random: " + (temp.random != null ? temp.random.val : "null"));
+            temp = temp.next;
+        }
+    }
+}
