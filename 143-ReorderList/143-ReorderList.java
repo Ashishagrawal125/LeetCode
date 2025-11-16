b8083627602f771fb54472be006ddba603637a89
Ashish Agrawal <143439220+Ashishagrawal125@users.noreply.github.com>
2025-11-16 23:50:02 +0530

    Create 143-ReorderList

---
+++ 143-ReorderList/143-ReorderList.java
@@ -0,0 +1,44 @@
+// Last updated: 11/16/2025, 11:50:01 PM
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null) return;
+        
+        ListNode slow = head, fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        ListNode second = slow.next;
+        slow.next = null;
+        
+        ListNode prev = null, curr = second;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        second = prev;
+        
+        ListNode first = head;
+        while (second != null) {
+            ListNode tmp1 = first.next;
+            ListNode tmp2 = second.next;
+            first.next = second;
+            second.next = tmp1;
+            first = tmp1;
+            second = tmp2;
+        }
+    }
+}
