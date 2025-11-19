fcf7d19002b31dfe3aa4bc7e10171c64995a2ac5
Ashish Agrawal <143439220+Ashishagrawal125@users.noreply.github.com>
2025-11-19 19:11:17 +0530

    Create 309-BestTimetoBuyandSellStockwithCooldown

---
+++ 309-BestTimetoBuyandSellStockwithCooldown/309-BestTimetoBuyandSellStockwithCooldown.java
@@ -0,0 +1,23 @@
+// Last updated: 11/19/2025, 7:11:17 PM
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        if (n < 2) return 0;
+
+        int hold = -prices[0];
+        int sold = 0;
+        int rest = 0;
+
+        for (int i = 1; i < n; i++) {
+            int prevHold = hold;
+            int prevSold = sold;
+            int prevRest = rest;
+
+            hold = Math.max(prevHold, prevRest - prices[i]);
+            sold = prevHold + prices[i];
+            rest = Math.max(prevRest, prevSold);
+        }
+
+        return Math.max(sold, rest);
+    }
+}
