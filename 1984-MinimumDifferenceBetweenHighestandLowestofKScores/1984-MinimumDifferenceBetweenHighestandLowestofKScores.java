44a7eab45aee8a2b505b7a8a08572d9ad5b08d4d
Ashish Agrawal <143439220+Ashishagrawal125@users.noreply.github.com>
2026-01-25 21:47:56 +0530

    Create 1984-MinimumDifferenceBetweenHighestandLowestofKScores

---
+++ 1984-MinimumDifferenceBetweenHighestandLowestofKScores/1984-MinimumDifferenceBetweenHighestandLowestofKScores.java
@@ -0,0 +1,13 @@
+// Last updated: 1/25/2026, 9:47:56 PM
+1class Solution {
+2
+3    public int minimumDifference(int[] nums, int k) {
+4        int n = nums.length;
+5        Arrays.sort(nums);
+6        int ans = Integer.MAX_VALUE;
+7        for (int i = 0; i + k - 1 < n; ++i) {
+8            ans = Math.min(ans, nums[i + k - 1] - nums[i]);
+9        }
+10        return ans;
+11    }
+12}
