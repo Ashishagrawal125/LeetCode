995a73b0bfaf6dd49ecfbeded0c6406d15818fa3
Ashish Agrawal <143439220+Ashishagrawal125@users.noreply.github.com>
2026-02-17 13:17:58 +0530

    File updated at 2/17/2026, 1:17:57 PM

--- 401-BinaryWatch/401-BinaryWatch.java
+++ 401-BinaryWatch/401-BinaryWatch.java
@@ -1,17 +1,18 @@
-// Last updated: 8/27/2025, 12:53:10 PM
-import java.util.*;
-
-public class Solution {
-    public List<String> readBinaryWatch(int turnedOn) {
-        List<String> result = new ArrayList<>();
-        for (int h = 0; h < 12; h++) {
-            for (int m = 0; m < 60; m++) {
-                int bitsOn = Integer.bitCount(h) + Integer.bitCount(m);
-                if (bitsOn == turnedOn) {
-                    result.add(String.format("%d:%02d", h, m));
-                }
-            }
-        }
-        return result;
-    }
-}
+// Last updated: 2/17/2026, 1:17:57 PM
+1import java.util.*;
+2
+3public class Solution {
+4    public List<String> readBinaryWatch(int turnedOn) {
+5        List<String> result = new ArrayList<>();
+6        for (int h = 0; h < 12; h++) {
+7            for (int m = 0; m < 60; m++) {
+8                int bitsOn = Integer.bitCount(h) + Integer.bitCount(m);
+9                if (bitsOn == turnedOn) {
+10                    result.add(String.format("%d:%02d", h, m));
+11                }
+12            }
+13        }
+14        return result;
+15    }
+16}
+17
