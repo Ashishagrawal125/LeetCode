3d4d267b751ba3af41fd9455299ac623f1193077
Ashish Agrawal <143439220+Ashishagrawal125@users.noreply.github.com>
2026-04-14 16:44:55 +0530

    Create 375-GuessNumberHigherOrLowerIi

---
+++ 375-GuessNumberHigherOrLowerIi/375-GuessNumberHigherOrLowerIi.java
@@ -0,0 +1,16 @@
+// Last updated: 4/14/2026, 4:44:54 PM
+class Solution {
+    public int getMoneyAmount(int n) {
+        int[][] dp = new int[n + 2][n + 2];
+        for (int len = 2; len <= n; len++) {
+            for (int l = 1; l + len - 1 <= n; l++) {
+                int r = l + len - 1;
+                dp[l][r] = Integer.MAX_VALUE;
+                for (int x = l; x <= r; x++) {
+                    dp[l][r] = Math.min(dp[l][r], x + Math.max(dp[l][x - 1], dp[x + 1][r]));
+                }
+            }
+        }
+        return dp[1][n];
+    }
+}
