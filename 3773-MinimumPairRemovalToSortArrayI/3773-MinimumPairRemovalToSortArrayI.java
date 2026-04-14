982de69a9e29f3e8cc4f748ad3e12f589e3ed9f5
Ashish Agrawal <143439220+Ashishagrawal125@users.noreply.github.com>
2026-04-14 16:42:24 +0530

    Create 3773-MinimumPairRemovalToSortArrayI

---
+++ 3773-MinimumPairRemovalToSortArrayI/3773-MinimumPairRemovalToSortArrayI.java
@@ -0,0 +1,40 @@
+// Last updated: 4/14/2026, 4:42:23 PM
+class Solution {
+
+    public int minimumPairRemoval(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        for (int num : nums) {
+            list.add(num);
+        }
+        var count = 0;
+
+        while (list.size() > 1) {
+            var isAscending = true;
+            var minSum = Integer.MAX_VALUE;
+            var targetIndex = -1;
+
+            for (var i = 0; i < list.size() - 1; i++) {
+                var sum = list.get(i) + list.get(i + 1);
+
+                if (list.get(i) > list.get(i + 1)) {
+                    isAscending = false;
+                }
+
+                if (sum < minSum) {
+                    minSum = sum;
+                    targetIndex = i;
+                }
+            }
+
+            if (isAscending) {
+                break;
+            }
+
+            count++;
+            list.set(targetIndex, minSum);
+            list.remove(targetIndex + 1);
+        }
+
+        return count;
+    }
+}
