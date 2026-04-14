b3137feab990e78799b8739263d243c9566ecf77
Ashish Agrawal <143439220+Ashishagrawal125@users.noreply.github.com>
2026-04-14 16:42:03 +0530

    Create 3958-MinimumRemovalsToBalanceArray

---
+++ 3958-MinimumRemovalsToBalanceArray/3958-MinimumRemovalsToBalanceArray.java
@@ -0,0 +1,20 @@
+// Last updated: 4/14/2026, 4:42:02 PM
+class Solution {
+
+    public int minRemoval(int[] nums, int k) {
+        int n = nums.length;
+        Arrays.sort(nums);
+
+        int ans = n;
+        int right = 0;
+
+        for (int left = 0; left < n; left++) {
+            while (right < n && nums[right] <= (long) nums[left] * k) {
+                right++;
+            }
+            ans = Math.min(ans, n - (right - left));
+        }
+
+        return ans;
+    }
+}
